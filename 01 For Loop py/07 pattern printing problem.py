8327e65a8cd57b17799138c5ec6a69006b86e973
RohanSanjayPol <polrohan90@gmail.com>
2023-07-08 09:54:32 +0530

    pattern printing

---
+++ 01 For Loop py/07 pattern printing problem.py
@@ -0,0 +1,10 @@
+# print star for 5 times
+for i in range(5):
+    print('*',end='')
+
+print('print star pattern for five rows and five coulumns')   
+print()
+for i in range(5):
+    for j in range(5):
+        print("*",end='') 
+    print('')   
