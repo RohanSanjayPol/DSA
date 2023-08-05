6230804870b222518c9657014f910f53e9b13453
RohanSanjayPol <polrohan90@gmail.com>
2023-08-05 09:41:16 +0530

    advance pattern

---
+++ 01 For Loop py/30 advance pattern rinting 2.py
@@ -0,0 +1,7 @@
+# advance pattern printing 2
+for row in range(1,6):
+    for col in range(5-row):
+        print(' ',end=' ')
+    for col in range(row*2-1):
+        print('*',end=' ')
+    print()        
