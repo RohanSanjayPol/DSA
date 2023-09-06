08a184f005d3906803f87d459e66193ccc41bbe0
RohanSanjayPol <polrohan90@gmail.com>
2023-09-06 19:51:47 +0530

    reverse array

---
+++ 06 arrays/05 reverse the element without taking other array.py
@@ -0,0 +1,14 @@
+# reverse the element without taking any other element
+def reverse(arr):
+    i=0
+    j=len(arr)-1
+    while(i<j):
+        temp=arr[i]
+        arr[i]=arr[j]
+        arr[j]=temp
+        i+=1
+        j-=1
+    return arr
+arr=[1,2,3,4,5,6,7,8,9]
+r=reverse(arr)
+print(r)    
